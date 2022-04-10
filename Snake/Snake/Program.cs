77824297d9f9e5d7c0e519b4a30b48099eb74b5a
korneevra <roman.korneev@inbox.ru>
2022-04-10 16:05:58 +0300

    Create border line

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,38 +6,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(2, 3, '*');
-            //p1.Draw();
-            Point p2 = new Point(3, 4, '#');
-            //p2.Draw();
-            Point p3 = new Point(4, 5, '@');
-            Point p4 = new Point(5, 6, '$');
+            Console.SetBufferSize(120, 30);
 
-            List<int> newList = new();
-            newList.Add(6);
-            newList.Add(7);
-            newList.Add(9);
-            int x = newList[0];
-            int y = newList[1];
-            int z = newList[2];
+            HorizontalLine uplLine = new HorizontalLine(0, 119, 0, '+');
+            HorizontalLine downlLine = new HorizontalLine(0, 119, 29, '+');
+            VerticalLine leftlLine = new VerticalLine(0, 0, 29, '+');
+            VerticalLine rightlLine = new VerticalLine(119, 0, 29, '+');
+            uplLine.Draw();
+            downlLine.Draw();
+            leftlLine.Draw();
+            rightlLine.Draw();
 
-            foreach (int i in newList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List < Point > points = new List<Point>();
-            points.Add(p1);
-            points.Add(p2);
-            points.Add(p3);
-            points.Add(p4);
-            points[2].Draw();
-
-            foreach (Point i in points)
-            {
-                i.Draw();
-            }
-
+            Point p = new(5, 5, '*');
+            p.Draw();
+            Console.Read();
+            
             
 
         }
